1124105c3ded37463f1551206f3fbccb55811622
sahil josan <sahiljosan50@gmail.com>
2022-11-30 07:37:32 +0530

    update code

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages,setup
 
-def get_requirements():
-    """
-    This function will provide the list of libraries name that are required for this project
-    """
-    pass
+from typing import List
+
+requirement_file_name = "requirements.txt"
+hypen_e_dot = "-e ."
+
+## This function will provide the list of libraries name that are required for this project
+def get_requirements()->List(str):
+    with open(requirement_file_name) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+    if hypen_e_dot in requirement_list:
+        requirement_list.remove(hypen_e_dot)
+    return requirement_list
+
 
 setup(
     name = 'sensor',
